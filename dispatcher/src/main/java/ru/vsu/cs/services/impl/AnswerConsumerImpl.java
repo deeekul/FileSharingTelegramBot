d922f901b399186c7fd993179ac69b2dd6e24a8b
deekul <dima.kul22@mail.ru>
2024-07-28 17:48:05 +0300

    add reading from queue

---
+++ dispatcher/src/main/java/ru/vsu/cs/services/impl/AnswerConsumerImpl.java
@@ -0,0 +1,24 @@
+package ru.vsu.cs.services.impl;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import ru.vsu.cs.controllers.UpdateController;
+import ru.vsu.cs.services.AnswerConsumer;
+
+import static ru.vsu.cs.model.RabbitQueue.ANSWER_MESSAGE;
+
+@Service
+public class AnswerConsumerImpl implements AnswerConsumer {
+    private final UpdateController updateController;
+
+    public AnswerConsumerImpl(UpdateController updateController) {
+        this.updateController = updateController;
+    }
+
+    @Override
+    @RabbitListener(queues = ANSWER_MESSAGE)
+    public void consume(SendMessage sendMessage) {
+        updateController.setView(sendMessage);
+    }
+}
