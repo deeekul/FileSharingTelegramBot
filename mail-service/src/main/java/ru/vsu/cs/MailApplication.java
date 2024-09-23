cb202bfee431405570caf913045f55b6b3539283
deekul <dima.kul22@mail.ru>
2024-09-23 19:19:20 +0300

    delete the parent pom.xml, convert modules to microservices

--- mail-service/src/main/java/ru/vsu/cs/MailApplication.java
+++ mail-service/src/main/java/ru/vsu/cs/MailApplication.java
@@ -2,12 +2,14 @@ package ru.vsu.cs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  *A microservice for sending emails with a link to confirm registration.
  * When clicking on the link, the user will send a GET request to the rest service,
  * which contains the logic for activating the user account
  */
+@ComponentScan("ru.vsu.cs.*")
 @SpringBootApplication
 public class MailApplication {
 
--- rest-service/src/main/java/ru/vsu/cs/RestService.java
+++ rest-service/src/main/java/ru/vsu/cs/RestService.java
@@ -2,10 +2,16 @@ package ru.vsu.cs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * The module is responsible for downloading files from the database via a link and activating the user account
  */
+@EnableJpaRepositories("ru.vsu.cs.*")
+@EntityScan("ru.vsu.cs.*")
+@ComponentScan("ru.vsu.cs.*")
 @SpringBootApplication
 public class RestService {
 
